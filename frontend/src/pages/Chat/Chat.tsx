0affd21433c388c89b8ee4a8550d40b199609c51
Katja Röhlig <katja-roehlig@mail.de>
2026-05-29 18:54:21 +0200

    Add chat style

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,33 +1,30 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { api } from "../../api/axios";
 import styles from "./Chat.module.css";
+import { CheckFatIcon } from "@phosphor-icons/react";
+import { useOutletContext } from "react-router-dom";
+import type { UserProfile } from "../../layouts/SerenityLayout";
 
 type ChatItem = { id: string; role: string; content: string };
 
 export const Chat = () => {
-  const userName = localStorage.getItem("userName");
+  const [userData, _getUserProfile] =
+    useOutletContext<[UserProfile, () => void]>();
+  const userName = userData.nickname;
   const [messages, setMessages] = useState<ChatItem[]>(() => {
-    const savedChat = localStorage.getItem("serenity-chat");
+    const savedChat = localStorage.getItem(`Chat ${userData.id}`);
     if (savedChat) return JSON.parse(savedChat) as ChatItem[];
-    return [];
-  });
-  const [content, setContent] = useState("");
-  const [isWaiting, setIsWaiting] = useState(false);
-  const hasStarted = useRef(false);
-  const cursorRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (hasStarted.current) return;
-    hasStarted.current = true;
-    const savedChat = localStorage.getItem("serenity-chat");
-    if (!savedChat) {
-      const startMessage: ChatItem = {
+    return [
+      {
         id: Date.now().toString(),
         role: "assistant",
         content: `Hey ${userName}! Ich bin Serenity, dein persönlicher Coach. Wie geht es dir heute?`,
-      };
-      setMessages((prev) => [...prev, startMessage]);
-    }
-  }, []);
+      },
+    ];
+  });
+  const [content, setContent] = useState("");
+  const [isWaiting, setIsWaiting] = useState(false);
+  const cursorRef = useRef<HTMLInputElement>(null);
 
   const handleChat = (event: React.SubmitEvent) => {
     setIsWaiting(true);
@@ -40,7 +37,10 @@ export const Chat = () => {
       },
       updatedMessages = [...messages, newMessage];
     setMessages(updatedMessages);
-    localStorage.setItem("serenity-chat", JSON.stringify(updatedMessages));
+    localStorage.setItem(
+      `Chat ${userData.id}`,
+      JSON.stringify(updatedMessages),
+    );
     setContent("");
     send_data(updatedMessages, newMessage);
   };
@@ -54,7 +54,10 @@ export const Chat = () => {
       if (response.data && response.data.content) {
         const aiMessage = { ...response.data, id: Date.now().toString() };
         const allMessages = [...messages, aiMessage];
-        localStorage.setItem("serenity-chat", JSON.stringify(allMessages));
+        localStorage.setItem(
+          `Chat ${userData.id}`,
+          JSON.stringify(allMessages),
+        );
         setMessages(allMessages);
       } else {
         console.error("Error: The response from the server is incomplete");
@@ -70,40 +73,48 @@ export const Chat = () => {
   };
 
   return (
-    <main>
-      <ul className={styles.messageList}>
-        {messages?.map((message) => (
-          <li key={message.id}>
-            <div
-              className={`${styles.chatBubble}
-                ${message.role == "user" ? styles.userChat : styles.kiChat}`}
-            >
-              {message.content}
+    <main className={styles.chatContainer}>
+      <div className={styles.layoutWrapper}>
+        <ul className={styles.messageList}>
+          {messages?.map((message) => (
+            <li key={message.id}>
+              <div
+                className={`${styles.chatBubble}
+             ${message.role == "user" ? styles.userChat : styles.kiChat}`}
+              >
+                {message.content}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={handleChat} className={styles.formContainer}>
+          <label htmlFor="content" className={styles.label}>
+            User Message
+          </label>
+          <div className={styles.writingContainer}>
+            <div className={styles.warning}>
+              <textarea
+                className={styles.input}
+                name="content"
+                id="content"
+                value={content}
+                onChange={(event) => setContent(event.target.value)}
+                placeholder="Schreib etwas ..."
+                autoFocus
+              ></textarea>
             </div>
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={handleChat} className={styles.formContainer}>
-        <label htmlFor="content" className={styles.label}>
-          User Message
-        </label>
-        <textarea
-          className={styles.input}
-          name="content"
-          id="content"
-          value={content}
-          onChange={(event) => setContent(event.target.value)}
-          placeholder="Schreib etwas ..."
-          autoFocus
-        ></textarea>
-        <button
-          type="submit"
-          disabled={isWaiting ? true : false}
-          className={styles.submitButton}
-        >
-          ✔️
-        </button>
-      </form>
+
+            <button
+              type="submit"
+              disabled={isWaiting ? true : false}
+              className={styles.submitButton}
+            >
+              <CheckFatIcon weight="fill" size={24} />
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className={styles.spacer}></div>
     </main>
   );
 };
